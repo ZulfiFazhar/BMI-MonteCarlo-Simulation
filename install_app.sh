53f7874df8fca669d5639723770c9626c6314b1f
Zulfi Fadilah Azhar <zulfi.fazhar12@gmail.com>
2024-06-28 12:41:42 +0700

    first commit

---
+++ install_app.sh
@@ -0,0 +1,5 @@
+pip install virtualenv
+virtualenv env
+source ./env/Scripts/activate
+pip install -r Source_code/requirements.txt
+streamlit run Source_code/Streamlit-Dashboard.py
---
+++ run_app.sh
@@ -0,0 +1,2 @@
+source ./env/Scripts/activate
+streamlit run Source_code/Streamlit-Dashboard.py
